941f921e9ccd136229104e8f534e125d76600019
jun.audis5 <jun.audis5@gmail.com>
2025-02-14 18:52:19 +0800

    update content

--- ans/leetcode_75/1071. Greatest Common Divisor of Strings.py
+++ ans/leetcode_75/1071. Greatest Common Divisor of Strings.py
@@ -1,9 +1,10 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1[:min(len(str1), len(str2))] != str2[:min(len(str1), len(str2))]:
+        idx = min(len(str1), len(str2))
+        if str1[:idx] != str2[:idx]:
             return ''
-        for i in range(min(len(str1), len(str2)), 0, -1):
-            x = str1[:i]
-            if str1.replace(x, '') == '' and str2.replace(x, '') == '':
-                return x
+        for i in range(idx, 0, -1):
+            check = str1[:i]
+            if str1.replace(check, '') == '' and str2.replace(check, '') == '':
+                return check
         return ''
