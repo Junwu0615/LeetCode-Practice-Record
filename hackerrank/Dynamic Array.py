da00bd76a2e77343fdb79ac09e438b651858d37b
PC <jun.audis5@gmail.com>
2026-08-14 00:35:15 +0800

    update somethings

---
+++ hackerrank/Dynamic Array.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'dynamicArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def dynamicArray(n, queries):
+    # Write your code here
+    tmp = [[] for i in range(n)]
+    ret = []
+    ans = 0
+    for q, x, y in queries:
+        idx = (x ^ ans) % n
+        if q == 1:
+            tmp[idx] += [y]
+        elif q == 2:
+            ele = y % len(tmp[idx])
+            ans = tmp[idx][ele]
+            ret += [ans]
+    return ret
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = dynamicArray(n, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
