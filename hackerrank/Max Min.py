4ce90cc8f74daf096b35837211f13b10fccf2dd6
PC <jun.audis5@gmail.com>
2026-08-14 00:12:27 +0800

    update somethings

---
+++ hackerrank/Max Min.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    # Write your code here
+    arr.sort()
+    n = len(arr)
+    max_num = float("inf")
+    for idx in range(n-k+1):
+        curr = arr[idx : idx + k]
+        diff = curr[-1] - curr[0]
+        if diff < max_num:
+            max_num = diff
+        # print(curr)
+    return max_num
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
